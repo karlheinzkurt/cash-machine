d3ef9868e8a17259f092f435d44830cd69d68f7b
karlheinzkurt <s.lumma@gmail.com>
2020-07-13 12:12:42 +0200

    Functional version

--- cash-machine/src/cash_machine.rs
+++ cash-machine/src/cash_machine.rs
@@ -11,6 +11,25 @@ impl fmt::Display for CashMachineError {
   }
 }
 
+pub struct CashMachine {
+  pub notes: Vec<u32>,
+}
+
+impl CashMachine {
+  pub fn create(notes: Vec<u32>) -> CashMachine {
+    return CashMachine { notes: notes };
+  }
+
+  pub fn get(&self, amount: u32) -> Result<u32, CashMachineError> {
+    return Ok(self.notes.iter().fold(amount, |value, note| {
+      let count = std::cmp::min(value / note, u32::MAX);
+      let residual = value - note * count;
+      println!("Print {} notes of amount {}", count, note);
+      return residual;
+    }));
+  }
+}
+
 #[test]
 fn error_test() {
   assert_eq!(
@@ -22,32 +41,4 @@ fn error_test() {
       }
     )
   )
-}
-
-pub struct CashMachine {
-  pub stages: Vec<Box<dyn Fn(u32) -> u32>>,
-}
-
-impl CashMachine {
-  pub fn create(notes: Vec<u32>) -> CashMachine {
-    let mut cash_machine = CashMachine { stages: Vec::new() };
-    for n in notes {
-      cash_machine.stages.push(Box::new(move |value| {
-        let count = std::cmp::min(value / n, u32::MAX);
-        let residual = value - n * count;
-        println!("Print {} notes of amount {}", count, n);
-
-        return residual;
-      }));
-    }
-    return cash_machine;
-  }
-
-  pub fn get(&self, amount: u32) -> Result<u32, CashMachineError> {
-    let mut result: u32 = amount;
-    for s in &self.stages {
-      result = s(result);
-    }
-    return Ok(result);
-  }
 }
